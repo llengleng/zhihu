8fdedf2fbcf5a46dd71e751c56ce1fc74249fc62
juhuiguang <juhuiguang@foxmail.com>
2020-01-22 23:39:02 +0800

    增加@Service注解

--- src/main/java/com/scs/soft/zhihu/api/service/ipml/ColumnServiceImpl.java
+++ src/main/java/com/scs/soft/zhihu/api/service/ipml/ColumnServiceImpl.java
@@ -3,10 +3,11 @@ package com.scs.soft.zhihu.api.service.ipml;
 import com.scs.soft.zhihu.api.entity.ColumnItem;
 import com.scs.soft.zhihu.api.mapper.ColumnMapper;
 import com.scs.soft.zhihu.api.service.ColumnService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
-
+@Service
 public class ColumnServiceImpl implements ColumnService {
     @Resource
     private ColumnMapper columnMapper;
